4dd384ff15b60a322a5e8aa3cd5bb0b951684b96
Alexandre <alex.s.carreira@gmail.com>
2024-01-29 23:29:07 +0000

    Correção imports de environment nos serviços.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {environment as environmentDev} from "../../environments/environment.development";
-import {environment} from "../../environments/environment.development";
+import {environment} from "../../environments/environment";
 import {ServerResponse, Signup} from "../interfaces";
 
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {environment as environmentDev} from "../../environments/environment.development";
-import {environment} from "../../environments/environment.development";
+import {environment} from "../../environments/environment";
 import {
     Category,
     ProductData,
